f9f8d62ee7e8d82c680e39d0e2e31ca9704c8dd2
ilyesdhiaeddine <i.belmilooud@esi-sba.dz>
2021-02-26 19:09:18 +0100

    DataBase.kt

---
+++ app/src/main/java/com/si7ateck/dz/data/DataBase.kt
@@ -0,0 +1,46 @@
+package com.si7ateck.dz.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.si7ateck.dz.data.doctor.Doctor
+import com.si7ateck.dz.data.doctor.DoctorDatabaseDao
+import com.si7ateck.dz.data.donner.Donner
+import com.si7ateck.dz.data.donner.DonnerDatabaseDao
+import com.si7ateck.dz.data.pharmacy.Pharmacy
+import com.si7ateck.dz.data.pharmacy.PharmacyDatabaseDao
+
+class DataBase {
+    @Database(
+        entities = [Pharmacy::class, Doctor::class, Donner::class],
+        version = 1,
+        exportSchema = true
+    )
+    abstract class DataBase : RoomDatabase() {
+        abstract val pharmacyDatabaseDao: PharmacyDatabaseDao
+        abstract val donnerDatabaseDao: DonnerDatabaseDao
+        abstract val doctorDatabaseDao: DoctorDatabaseDao
+
+        companion object {
+            @Volatile
+            private var INSTANCE: DataBase? = null
+
+            fun getInstance(context: Context): DataBase {
+                synchronized(this) {
+                    var instance = INSTANCE
+                    if (instance == null) {
+                        instance = Room.databaseBuilder(
+                            context.applicationContext,
+                            DataBase::class.java,
+                            "database"
+                        ).fallbackToDestructiveMigration()
+                            .build()
+                        INSTANCE = instance
+                    }
+                    return instance
+                }
+            }
+        }
+    }
+}
